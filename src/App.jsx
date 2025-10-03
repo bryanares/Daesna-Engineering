9f716a04c58acca5668369c012442f4cb5b2f562
bryanares <bryanares12@gmail.com>
2025-10-03 16:45:27 +0300

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import DroneClub from "./components/Droneclub"
 import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
